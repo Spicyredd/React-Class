2bcd15103e6d52b2001a919a652a91031d9d0dbe
rishavbeejukchhen <rbeeju20@gmail.com>
2024-08-14 13:05:43 +0545

    Added files again

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Details = () => {
   return (
     <div>
-      <p>Name: Rishav Beejukchhen</p>
-      <p>Mother's Name: Arjun Bahadur Beejukchhen</p>
-      <p>Father's Name: Rita Rajbahak</p>
+      <p style={{ backgroundColor: "red", color: "white" }}>
+        Name: Rishav Beejukchhen
+      </p>
+      <p style={{ backgroundColor: "yellow", color: "blue" }}>
+        Father's Name: Arjun Bahadur Beejukchhen
+      </p>
+      <p style={{ backgroundColor: "green", color: "cyan" }}>
+        Mother's Name: Rita Rajbahak
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Details
+export default Details;
